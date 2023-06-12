1a2fb4a891b3f59f728ffd6b0b1727ae07435297
Quentin <quentin@quentin-linux.fr>
2023-06-12 17:53:20 +0200

    add esp now

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <Arduino.h>
 
-//Variables of the pins
+#include <esp_now.h>
+#include <WiFi.h>
+
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+  char a[32];
+  int erreur;
+  int timer;
+  int timer_value;
+  int module;
+  int difficulty;
+  int level[2];
+  int condo;
+  char num_serie[6];
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+}
+
+// Variables of the pins
 const int A1 = 16;
 const int B_1 = 13;
 const int C1 = 32;
@@ -19,20 +56,20 @@ const int B4 = 2;
 const int C4 = 0;
 const int D4 = 4;
 
-//Array of the pins
+// Array of the pins
 const int pin_segment[4][5] = {
     {A1, B_1, C1, D1, dp1},
     {A2, B2, C2, D2, 0},
     {D3, C3, B3, A_3, 0},
     {A_4, B4, C4, D4, 0}};
 
-//Prototypes of the functions
+// Prototypes of the functions
 void number_digit(int num_segment, int numeral);
 void timer(int timer_value);
 void test_number(bool activate);
 void test_place(bool activate);
 
-//Variables of the timer
+// Variables of the timer
 unsigned long previousMillis = 0;
 int sec_unite = 0;
 int sec_dizaine = 0;
@@ -59,6 +96,37 @@ void setup()
   pinMode(5, OUTPUT);
   pinMode(18, OUTPUT);
   pinMode(19, OUTPUT);
+
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  Serial.println(WiFi.macAddress());
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
 
   test_number(true);
   test_place(true);
@@ -135,7 +203,8 @@ void number_digit(int num_segment, int numeral)
 }
 */
 
-void timer(int timer_value){
+void timer(int timer_value)
+{
   /*
   This function allows you to change the value of the variables:
   minutes, sec_ten and sec_unit in order to make the timer work
